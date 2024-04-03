5644f6616a623f3c4a9c4c9653727f5fd0950639
Naveen <nerellanaveen89@gmail.com>
2024-04-03 22:47:56 +0530

    Finished Outlining Website
    Finished core of the website

--- scripts/product.js
+++ scripts/product.js
@@ -1,25 +1,75 @@
-const carousel = document.querySelector(".cauresal");
-const slidesContainer = document.querySelector(".slider");
-const prevButton = document.querySelector(".btn_prev");
-const nextButton = document.querySelector(".btn_next");
+new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const media = window.matchMedia("(width < 40em)");
+const topNavMenu = document.querySelector(".topnav_menu");
+const main = document.querySelector("main");
+const body = document.querySelector("body");
 
-const slides = Array.from(slidesContainer.children);
-let currentIndex = 0;
-
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${100 * (i - index)}%)`;
-  });
+function openMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "true");
+  topNavMenu.removeAttribute("inert");
+  topNavMenu.removeAttribute("style");
+  main.setAttribute("inert", "");
+  bodyScrollLockUpgrade.disableBodyScroll(body);
+  btnClose.focus();
 }
 
-prevButton.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  showSlide(currentIndex);
-});
+function closeMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "false");
+  topNavMenu.setAttribute("inert", "");
+  main.removeAttribute("inert");
+  bodyScrollLockUpgrade.enableBodyScroll(body);
+  btnOpen.focus();
 
-nextButton.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
-});
+  setTimeout(() => {
+    topNavMenu.style.transition = "none";
+  }, 500);
+}
 
-showSlide(currentIndex);
+function setupTopNav(e) {
+  if (e.matches) {
+    // is mobile
+    console.log("is mobile");
+    topNavMenu.setAttribute("inert", "");
+    topNavMenu.style.transition = "none";
+  } else {
+    // is tablet/desktop
+    console.log("is desktop");
+    closeMobileMenu();
+    topNavMenu.removeAttribute("inert");
+  }
+}
+
+setupTopNav(media);
+
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", closeMobileMenu);
+
+media.addEventListener("change", function (e) {
+  setupTopNav(e);
+});
